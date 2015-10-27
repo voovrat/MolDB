ff6d24c220465986b86b1d841c79c307ecb5361d
Volodymyr Sergiievskyi aka voovrat <voov.rat@gmail.com>
2015-10-27 15:44:19 +0100

    imodified

--- Projects/Clusters/Prototypes/gen_cluster.py
+++ Projects/Clusters/Prototypes/gen_cluster.py
@@ -8,14 +8,18 @@ if len(sys.argv)<5:
 c=XYZ()
 c.read(sys.argv[1])
 
-sc_rest = XYZ()
-sc_rest.read(sys.argv[2])
+if sys.argv[3] == 'empty':
+  cc = c
+else:
+  sc_rest = XYZ()
+  sc_rest.read(sys.argv[2])
 
-words = str.replace(sys.argv[3],'c',' ').split()
-oxl = [] #sc_rest.oxylist()
-for w in words:
-   oxl.append( sc_rest.oxylist()[int(w)])
+  words = str.replace(sys.argv[3],'c',' ').split()
+  oxl = [] #sc_rest.oxylist()
+  for w in words:
+     oxl.append( sc_rest.oxylist()[int(w)])
 
-cc = c + sc_rest.select_oxygens(oxl)
+  cc = c + sc_rest.select_oxygens(oxl)
+
 cc.write(sys.argv[4])
 
